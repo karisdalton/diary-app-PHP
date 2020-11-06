ed09ef5a0becbc8b578f22e9aa84308d0beacd4c
karis dalton <58391690+karisdalton@users.noreply.github.com>
2020-11-06 18:05:34 +0300

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,7 +1,7 @@
 <?php 
 
     // connect to database
-    $conn = mysqli_connect('localhost', 'your database username default is root', 'database password if you have one otherwise leave balnk', 'database name');
+    $conn = mysqli_connect('localhost', 'your database username default is root', 'database password if you have one otherwise leave blank', 'database name');
 
     // check connection
     if (!$conn) {
